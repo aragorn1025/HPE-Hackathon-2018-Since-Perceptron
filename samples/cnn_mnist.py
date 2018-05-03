77277c0b3638fb54e96a53ec9c7bdaaa39278232
Aragorn Dai <tzjie.dai@gmail.com>
2018-05-03 20:51:46 +0800

    Modify temporary path

--- samples/cnn_mnist.py
+++ samples/cnn_mnist.py
@@ -13,23 +13,29 @@ from keras.utils import np_utils
 
 print()
 
-temp_dir = './temp/'
-if os.path.exists(temp_dir):
-    shutil.rmtree(temp_dir)
-    os.makedirs(temp_dir)
-    print('Clear the directory: {}\n'.format(temp_dir))
-else:
-    os.makedirs(temp_dir)
-    print('Create the directory: {}\n'.format(temp_dir))
+temp_dir = "../temp/"
+
+def init_dir(dir):
+    try:
+        if os.path.exists(dir):
+            shutil.rmtree(dir)
+            os.makedirs(dir)
+            print("Clear the directory: {}".format(dir))
+        else:
+            os.makedirs(dir)
+            print("Create the directory: {}".format(dir))
+    except OSError:
+        init_dir(dir)
+init_dir(temp_dir)
 
 def savefig(fig, file_name):
-    fig.savefig(temp_dir + file_name + '.png')
-    print('Image file saved:', temp_dir + file_name + '.png')
+    fig.savefig(temp_dir + file_name + ".png")
+    print("Image file saved:", temp_dir + file_name + ".png")
 
-print('Loading data...')
+print("Loading data...")
 from keras.datasets import mnist
 (x_train_image, y_train_label), (x_test_image, y_test_label) = mnist.load_data()
-print('Done')
+print("Done")
 print("x_train_image.shape = {}".format(x_train_image.shape))
 print("y_train_label.shape = {}".format(y_train_label.shape))
 print(" x_test_image.shape = {}".format(x_test_image.shape))
@@ -50,7 +56,7 @@ def plot_images_labels_prediction(images, labels,
         raise ValueError("The maximum data number to print is " + str(images_per_row * max_row_number) + ".")
     for i in range(0, num):
         ax = plt.subplot(max_row_number, images_per_row, 1 + i)
-        ax.imshow(images[start_index], cmap='binary')
+        ax.imshow(images[start_index], cmap="binary")
         title = "i = " + str(start_index) + ", y = " + str(labels[start_index])
         if len(prediction) > 0:
             title += ", predict = " + str(prediction[start_index])
@@ -60,16 +66,16 @@ def plot_images_labels_prediction(images, labels,
         start_index += 1
 #   plt.show()
     if len(prediction) <= 0:
-        savefig(fig, 'images_train_' + str(start_index - num) + '_' + str(start_index - 1))
+        savefig(fig, "cnn_images_train_" + str(start_index - num) + "_" + str(start_index - 1))
     else:
-        savefig(fig, 'images_test_' + str(start_index - num) + '_' + str(start_index - 1))
+        savefig(fig, "cnn_images_test_" + str(start_index - num) + "_" + str(start_index - 1))
 
 plot_images_labels_prediction(x_train_image, y_train_label)
 print()
 
 # normalize input image, 0 < input < 1
 def normalize_input(input_image):
-    return input_image.reshape(input_image.shape[0], 28, 28, 1).astype('float64') / 255
+    return input_image.reshape(input_image.shape[0], 28, 28, 1).astype("float64") / 255
 
 # normalize output label as one-hot encoding, for example, 4 will normalize as [0. 0. 0. 0. 1. 0. 0. 0. 0. 0.]
 def normalize_output(output_label):
@@ -88,9 +94,9 @@ model.add(
     Conv2D(
         filters = 16, # the number of output filters
         kernel_size = (5, 5), # the width and height of the 2D convolution window
-        padding = 'same', # not changing data shape
+        padding = "same", # not changing data shape
         input_shape = (28, 28, 1),
-        activation = 'relu'
+        activation = "relu"
     )
 )
 # pooling layer 1
@@ -103,9 +109,9 @@ model.add(
 model.add(
     Conv2D(filters = 36,
         kernel_size = (5, 5),
-            padding = 'same',
+            padding = "same",
             input_shape = (28, 28, 1),
-            activation = 'relu'
+            activation = "relu"
     )
 )
 # pooling layer 2
@@ -128,30 +134,30 @@ model.add(
 model.add(
     Dense(
         units = 100,
-        activation = 'relu',
-        kernel_initializer = 'normal',
-        bias_initializer = 'normal'
+        activation = "relu",
+        kernel_initializer = "normal",
+        bias_initializer = "normal"
     )
 )
 # hidden layer 2
 model.add(
     Dense(
         units = 10,
-        activation = 'softmax',
-        kernel_initializer = 'normal',
-        bias_initializer = 'normal'
+        activation = "softmax",
+        kernel_initializer = "normal",
+        bias_initializer = "normal"
     )
 )
-print(model.summary(), '\n')
+print(model.summary(), "\n")
 
 # definite training method
 model.compile(
-    optimizer = 'adam',
-    loss = 'categorical_crossentropy',
-    metrics = ['accuracy']
+    optimizer = "adam",
+    loss = "categorical_crossentropy",
+    metrics = ["accuracy"]
 )
 
-# start training
+# start training, by TensorFlow for CPU
 train_history = model.fit(
     x = x_train,
     y = y_train,
@@ -164,33 +170,33 @@ def show_train_history(train_history, train, validation):
     plt.clf()
     plt.plot(train_history.history[train])
     plt.plot(train_history.history[validation])
-    plt.title('Train History')
+    plt.title("Train History")
     plt.ylabel(train)
-    plt.xlabel('Epoch')
-    plt.legend(['train', 'validation'], loc = 'upper left')
+    plt.xlabel("Epoch")
+    plt.legend(["train", "validation"], loc = "upper left")
 #   plt.show()
-    savefig(plt, 'training_history_' + train)
+    savefig(plt, "cnn_training_history_" + train)
 
-show_train_history(train_history, 'acc', 'val_acc')
-show_train_history(train_history, 'loss', 'val_loss')
+show_train_history(train_history, "acc", "val_acc")
+show_train_history(train_history, "loss", "val_loss")
 print()
 
-print('Evaluating: ')
+print("Evaluating: ")
 scores = model.evaluate(x_test, y_test)
 print("The loss of testing data:", scores[0])
 print("The accuracy of testing data = {}%".format(scores[1] * 100.0))
 
-print('Predicting: ')
+print("Predicting: ")
 prediction = model.predict_classes(x_test, verbose = 1)
-print(pd.crosstab(y_test_label, prediction, colnames=['predict'], rownames=['y']), '\n')
+print(pd.crosstab(y_test_label, prediction, colnames=["predict"], rownames=["y"]), "\n")
 
-print('Data frame for error cases:')
-df = pd.DataFrame({'label': y_test_label, 'predict': prediction})
+print("Data frame for error cases:")
+df = pd.DataFrame({"label": y_test_label, "predict": prediction})
 error_list = df.loc[df.label != df.predict]
-print(error_list, '\n')
+print(error_list, "\n")
 
 print("The error case is at i = {}".format(error_list.index[0] - 3))
 plot_images_labels_prediction(x_test_image, y_test_label, prediction, start_index = max(0, error_list.index[0] - 3))
 print()
 
-print('Process ended.', '\n')
+print("Process ended.", "\n")
--- samples/mlp_mnist.py
+++ samples/mlp_mnist.py
@@ -9,23 +9,29 @@ from keras.utils import np_utils
 
 print()
 
-temp_dir = './temp/'
-if os.path.exists(temp_dir):
-    shutil.rmtree(temp_dir)
-    os.makedirs(temp_dir)
-    print('Clear the directory: {}\n'.format(temp_dir))
-else:
-    os.makedirs(temp_dir)
-    print('Create the directory: {}\n'.format(temp_dir))
+temp_dir = "../temp/"
+
+def init_dir(dir):
+    try:
+        if os.path.exists(dir):
+            shutil.rmtree(dir)
+            os.makedirs(dir)
+            print("Clear the directory: {}".format(dir))
+        else:
+            os.makedirs(dir)
+            print("Create the directory: {}".format(dir))
+    except OSError:
+        init_dir(dir)
+init_dir(temp_dir)
 
 def savefig(fig, file_name):
-    fig.savefig(temp_dir + file_name + '.png')
-    print('Image file saved:', temp_dir + file_name + '.png')
+    fig.savefig(temp_dir + file_name + ".png")
+    print("Image file saved:", temp_dir + file_name + ".png")
 
-print('Loading data...')
+print("Loading data...")
 from keras.datasets import mnist
 (x_train_image, y_train_label), (x_test_image, y_test_label) = mnist.load_data()
-print('Done')
+print("Done")
 print("x_train_image.shape = {}".format(x_train_image.shape))
 print("y_train_label.shape = {}".format(y_train_label.shape))
 print(" x_test_image.shape = {}".format(x_test_image.shape))
@@ -46,7 +52,7 @@ def plot_images_labels_prediction(images, labels,
         raise ValueError("The maximum data number to print is " + str(images_per_row * max_row_number) + ".")
     for i in range(0, num):
         ax = plt.subplot(max_row_number, images_per_row, 1 + i)
-        ax.imshow(images[start_index], cmap='binary')
+        ax.imshow(images[start_index], cmap="binary")
         title = "i = " + str(start_index) + ", y = " + str(labels[start_index])
         if len(prediction) > 0:
             title += ", predict = " + str(prediction[start_index])
@@ -56,16 +62,16 @@ def plot_images_labels_prediction(images, labels,
         start_index += 1
 #   plt.show()
     if len(prediction) <= 0:
-        savefig(fig, 'images_train_' + str(start_index - num) + '_' + str(start_index - 1))
+        savefig(fig, "mlp_images_train_" + str(start_index - num) + "_" + str(start_index - 1))
     else:
-        savefig(fig, 'images_test_' + str(start_index - num) + '_' + str(start_index - 1))
+        savefig(fig, "mlp_images_test_" + str(start_index - num) + "_" + str(start_index - 1))
 
 plot_images_labels_prediction(x_train_image, y_train_label)
 print()
 
 # normalize input image, 0 < input < 1
 def normalize_input(input_image):
-    return input_image.reshape(input_image.shape[0], 28 * 28).astype('float64') / 255
+    return input_image.reshape(input_image.shape[0], 28 * 28).astype("float64") / 255
 
 # normalize output label as one-hot encoding, for example, 4 will normalize as [0. 0. 0. 0. 1. 0. 0. 0. 0. 0.]
 def normalize_output(output_label):
@@ -84,9 +90,9 @@ model.add(
     Dense(
         units = 100,
         input_dim = 784,
-        activation = 'relu',
-        kernel_initializer = 'normal',
-        bias_initializer = 'normal'
+        activation = "relu",
+        kernel_initializer = "normal",
+        bias_initializer = "normal"
     )
 )
 # dropout layer
@@ -99,18 +105,18 @@ model.add(
 model.add(
     Dense(
         units = 10,
-        activation = 'softmax',
-        kernel_initializer = 'normal',
-        bias_initializer = 'normal'
+        activation = "softmax",
+        kernel_initializer = "normal",
+        bias_initializer = "normal"
     )
 )
-print(model.summary(), '\n')
+print(model.summary(), "\n")
 
 # definite training method
 model.compile(
-    optimizer = 'adam',
-    loss = 'categorical_crossentropy',
-    metrics = ['accuracy']
+    optimizer = "adam",
+    loss = "categorical_crossentropy",
+    metrics = ["accuracy"]
 )
 
 # start training
@@ -126,33 +132,33 @@ def show_train_history(train_history, train, validation):
     plt.clf()
     plt.plot(train_history.history[train])
     plt.plot(train_history.history[validation])
-    plt.title('Train History')
+    plt.title("Train History")
     plt.ylabel(train)
-    plt.xlabel('Epoch')
-    plt.legend(['train', 'validation'], loc = 'upper left')
+    plt.xlabel("Epoch")
+    plt.legend(["train", "validation"], loc = "upper left")
 #   plt.show()
-    savefig(plt, 'training_history_' + train)
+    savefig(plt, "mlp_training_history_" + train)
 
-show_train_history(train_history, 'acc', 'val_acc')
-show_train_history(train_history, 'loss', 'val_loss')
+show_train_history(train_history, "acc", "val_acc")
+show_train_history(train_history, "loss", "val_loss")
 print()
 
-print('Evaluating: ')
+print("Evaluating: ")
 scores = model.evaluate(x_test, y_test)
 print("The loss of testing data:", scores[0])
 print("The accuracy of testing data = {}%".format(scores[1] * 100.0))
 
-print('Predicting: ')
+print("Predicting: ")
 prediction = model.predict_classes(x_test, verbose = 1)
-print(pd.crosstab(y_test_label, prediction, colnames=['predict'], rownames=['y']), '\n')
+print(pd.crosstab(y_test_label, prediction, colnames=["predict"], rownames=["y"]), "\n")
 
-print('Data frame for error cases:')
-df = pd.DataFrame({'label': y_test_label, 'predict': prediction})
+print("Data frame for error cases:")
+df = pd.DataFrame({"label": y_test_label, "predict": prediction})
 error_list = df.loc[df.label != df.predict]
-print(error_list, '\n')
+print(error_list, "\n")
 
 print("The error case is at i = {}".format(error_list.index[0] - 3))
 plot_images_labels_prediction(x_test_image, y_test_label, prediction, start_index = max(0, error_list.index[0] - 3))
 print()
 
-print('Process ended.', '\n')
+print("Process ended.", "\n")
