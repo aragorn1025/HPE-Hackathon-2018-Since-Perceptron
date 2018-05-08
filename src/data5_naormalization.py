5ac98a4de443f629bfc68381b195774dcdc10de2
KoI <koikoi1282@gmail.com>
2018-05-08 20:26:05 +0800

    Create data5_normalization

--- src/data5_naormalization.py
+++ src/data5_naormalization.py
@@ -18,7 +18,7 @@ else:
     print('Create the directory: {}\n'.format(temp_dir))
 
 # open file
-loadfile = '../data/data6.xlsx'
+loadfile = '../data/data5.xlsx'
 loadbook = load_workbook(loadfile)
 # get all sheets in the file
 sheets = loadbook.get_sheet_names()
@@ -26,7 +26,7 @@ sheets = loadbook.get_sheet_names()
 sheet0 = sheets[0]
 
 # create new file
-writefile = './temp/data6_normalization.xlsx'
+writefile = './temp/data5_normalization.xlsx'
 writebook = Workbook()
 
 # get sheets
@@ -34,16 +34,21 @@ loadsheet = loadbook.get_sheet_by_name(name = sheet0)
 writesheet = writebook.active
 
 # normalize
-normalization_dictionary, normalize = normalization(loadsheet, 2, 4, 4)
-starttimenormalize = time_normalization_datetime(loadsheet, 2, 5, 5)
-endtimenormalize = time_normalization_datetime(loadsheet, 2, 6, 6)
 
-print(normalization_dictionary)
+starttimenormalize = time_normalization_datestr(loadsheet, 3, 1, 1)
+owner_dictionary, ownernormalize = normalization(loadsheet, 3, 2, 2)
+type_dictionary, typenormalize = normalization(loadsheet, 3, 4, 4)
+reason_dictionary, reasonnormalize = normalization(loadsheet, 3, 5, 5)
+endtimenormalize = time_normalization_datestr(loadsheet, 3, 6, 6)
+
+print(owner_dictionary)
 
 # write into sheet
-write(writesheet, normalize, 1)
-write(writesheet, starttimenormalize, 2)
-write(writesheet, endtimenormalize, 3)
+write(writesheet, starttimenormalize, 1)
+write(writesheet, ownernormalize, 2)
+write(writesheet, typenormalize, 3)
+write(writesheet, reasonnormalize, 4)
+write(writesheet, endtimenormalize, 5)
 
 # save file
 writebook.save(writefile)
--- src/normalization_function.py
+++ src/normalization_function.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import time
+import types
 
 def normalization (loadsheet, min_row, max_col, min_col):
     content = []
@@ -10,16 +11,17 @@ def normalization (loadsheet, min_row, max_col, min_col):
             
     dic = []
     match = False
-    dic.append('None')
+    dic.append(None)
 
 # get all value in content
     for value in content:
-        for item in dic:
-            if value == item:
-                match = True
-                break
-        if match == False:
-            dic.append(value)
+        if value != None:
+            for item in dic:
+                if value == item:
+                    match = True
+                    break
+            if match == False:
+                dic.append(value)
         match = False
 
 # normalize
@@ -27,7 +29,7 @@ def normalization (loadsheet, min_row, max_col, min_col):
     for value in content:
         for item in dic:
             if value == item:
-                normalize.append(dic.index(item))
+                normalize.append(dic.index(item)/len(dic))
 
 # make dictionary
     dictionary = {}
@@ -38,18 +40,39 @@ def normalization (loadsheet, min_row, max_col, min_col):
 
     return dictionary, normalize
 
-def time_normalization(loadsheet, min_row, max_col, min_col):
+def time_normalization_datetime(loadsheet, min_row, max_col, min_col):
     timecontent = []
     for col in loadsheet.iter_cols(min_row = min_row, max_col = max_col, min_col = min_col):
         for cell in col:
             timecontent.append(cell.value)
-
+            
+    print(type(timecontent[0]))
+        
     timenormalize = []
     for value in timecontent:
         time_tuple = value.timetuple()
         ts = time.mktime(time_tuple)
         timenormalize.append(ts)
+
+    return timenormalize
+
+def time_normalization_datestr(loadsheet, min_row, max_col, min_col):
+    timecontent = []
+    for col in loadsheet.iter_cols(min_row = min_row, max_col = max_col, min_col = min_col):
+        for cell in col:
+            timecontent.append(cell.value)
+            
+    print(type(timecontent[0]))
         
+    timenormalize = []
+    for value in timecontent:
+        if isinstance (value, str):
+            t_obj = time.strptime(value, "%Y.%m.%d %H:%M")
+            ts = time.mktime(t_obj)
+            timenormalize.append(ts)
+        else:
+            timenormalize.append(0)
+
     return timenormalize
 
 def write(writesheet, content, col):
