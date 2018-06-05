d958dd0e0eca083d9497a8f1b1571e46f9a71841
AragornDai <tzjie.dai@gmail.com>
2018-06-05 16:01:13 +0800

    Data

---
+++ data/get_extension.bat
@@ -0,0 +1,9 @@
+@echo on
+rename data0 data0.xlsx
+rename data1 data1.xlsx
+rename data2 data2.xlsx
+rename data3 data3.xlsx
+rename data4 data4.xlsx
+rename data5 data5.xlsx
+rename data6 data6.xlsx
+rename data7 data7.xlsx
---
+++ data/remove_extension.bat
@@ -0,0 +1,9 @@
+@echo on
+rename data0.xlsx data0
+rename data1.xlsx data1
+rename data2.xlsx data2
+rename data3.xlsx data3
+rename data4.xlsx data4
+rename data5.xlsx data5
+rename data6.xlsx data6
+rename data7.xlsx data7
